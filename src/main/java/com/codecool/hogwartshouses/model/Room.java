a3901bcf8384b208788de6115967401d8af11a29
T.Sanyi <s.truczkai@gmail.com>
2023-02-18 14:16:52 +0100

    rework addStudent: students are only added if the room is not full, and student is in same house

--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -13,19 +13,21 @@ public class Room {
 
     private final static int ROOM_CAPACITY = 3;
     private static int count;
-    @Builder.Default private final int id = ++count;
-    @Singular private final Set<Student> students = new HashSet<>();
+    @Builder.Default
+    private final int id = ++count;
+    @Singular
+    private final Set<Student> students = new HashSet<>();
     private final HouseType houseType;
     private RoomCondition roomCondition;
     private boolean isFull = false;
 
-    public void addStudent(Student student){
-        students.add(student);
+    public void addStudent(Student student) {
+        if (!isFull && student.getHouseType().equals(houseType)) students.add(student);
         checkIfFull();
     }
 
-    private void checkIfFull(){
-        if(students.size() == ROOM_CAPACITY)
+    private void checkIfFull() {
+        if (students.size() == ROOM_CAPACITY)
             this.isFull = true;
     }
 }
