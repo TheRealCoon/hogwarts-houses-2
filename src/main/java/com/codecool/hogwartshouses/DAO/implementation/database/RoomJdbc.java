2d4d6f5938514377b12cb72d39b4dd84e393e4fa
T.Sanyi <s.truczkai@gmail.com>
2023-03-17 16:43:14 +0100

    add spring annotations

--- src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/RoomJdbc.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/RoomJdbc.java
@@ -3,10 +3,14 @@ package com.codecool.hogwartshouses.DAO.implementation.database;
 import com.codecool.hogwartshouses.DAO.RoomDAO;
 import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.model.types.PetType;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
+@Repository
+@Primary
 public class RoomJdbc implements RoomDAO {
     private final JdbcTemplate template;
     //TODO add mapper
--- src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/StudentJdbc.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/StudentJdbc.java
@@ -2,8 +2,12 @@ package com.codecool.hogwartshouses.DAO.implementation.database;
 
 import com.codecool.hogwartshouses.DAO.StudentDAO;
 import com.codecool.hogwartshouses.model.Student;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Primary
 public class StudentJdbc implements StudentDAO {
     private final JdbcTemplate template;
     //TODO add mapper
