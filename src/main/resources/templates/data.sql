1eebc7b7821de7ff439e2b971b4c6783c70f40c5
T.Sanyi <s.truczkai@gmail.com>
2023-03-17 15:56:18 +0100

    add data to some tables

---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO building (NAME) values ('Building no.1');
+INSERT INTO building (NAME) values ('Building no.2');
+INSERT INTO building (NAME) values ('Building no.3');
+INSERT INTO building (NAME) values ('Building no.4');
+INSERT INTO building (NAME) values ('Building no.5');
+
+INSERT INTO room (building_id, house_type, room_condition) values (1, 'GRYFFINDOR', 'OLD');
+INSERT INTO room (building_id, house_type, room_condition) values (2, 'SLYTHERIN', 'MESSY');
+INSERT INTO room (building_id, house_type, room_condition) values (1, 'GRYFFINDOR', 'UGLY');
+INSERT INTO room (building_id, house_type, room_condition) values (3, 'HUFFLEPUFF', 'RENOVATED');
+INSERT INTO room (building_id, house_type, room_condition) values (1, 'HUFFLEPUFF', 'OLD');
+INSERT INTO room (building_id, house_type, room_condition) values (4, 'RAVENCLAW', 'UGLY');
+INSERT INTO room (building_id, house_type, room_condition) values (4, 'RAVENCLAW', 'UGLY');
+INSERT INTO room (building_id, house_type, room_condition) values (1, 'SLYTHERIN', 'OLD');
+
+INSERT INTO picture (building_id) values (1);
+INSERT INTO picture (building_id) values (1);
+INSERT INTO picture (building_id) values (2);
+INSERT INTO picture (building_id) values (2);
+INSERT INTO picture (building_id) values (2);
+INSERT INTO picture (building_id) values (3);
+INSERT INTO picture (building_id) values (4);
+INSERT INTO picture (building_id) values (4);
+INSERT INTO picture (building_id) values (4);
+INSERT INTO picture (building_id) values (4);
+INSERT INTO picture (building_id) values (5);
+
+
+
