3b100d8d0db64e1c038962ec486cd66f1e577b5f
T.Sanyi <s.truczkai@gmail.com>
2023-03-17 20:16:22 +0100

    implement most methods in RoomDaoJdbcImpl

--- src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/RoomDaoJdbcImpl.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/RoomDaoJdbcImpl.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Primary
@@ -21,29 +22,36 @@ public class RoomDaoJdbcImpl implements RoomDAO {
         this.mapper = mapper;
     }
 
+    //TODO need to join resident everywhere to list residents also
+
     @Override
     public List<Room> getAll() {
-        return null;
+        final String sql = "SELECT id, building_id, house_type, room_condition FROM room;";
+        return template.query(sql, mapper);
     }
 
     @Override
     public void add(Room room) {
-
+        final String sql = "INSERT INTO room (building_id, house_type, room_condition) VALUES (?, ?, ?);";
+        template.update(sql, room.getBuildingId(), room.getHouseType().toString(), room.getRoomCondition().toString());
     }
 
     @Override
-    public Room findById(int id) {
-        return null;
+    public Optional<Room> findById(long id) {
+        final String sql = "SELECT id, building_id, house_type, room_condition FROM room WHERE id = ?;";
+        return template.query(sql, mapper, id).stream().findFirst();
     }
 
     @Override
-    public void delete(int id) {
-
+    public void delete(long id) {
+        final String sql = "DELETE FROM room WHERE id = ?;";
+        template.update(sql, id);
     }
 
     @Override
-    public void renovate(int id) {
-
+    public void renovate(long id) {
+        final String sql = "UPDATE room SET room_condition = 'RENOVATED' WHERE id = ?;";
+        template.update(sql, id);
     }
 
     @Override
