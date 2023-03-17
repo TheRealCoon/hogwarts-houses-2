301a51df130231d76c5d79e1ac4a94f96838acad
T.Sanyi <s.truczkai@gmail.com>
2023-03-17 18:16:32 +0100

    add getAll() to StudentDAO interface

--- src/main/java/com/codecool/hogwartshouses/DAO/StudentDAO.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/StudentDAO.java
@@ -3,8 +3,12 @@ package com.codecool.hogwartshouses.DAO;
 import com.codecool.hogwartshouses.model.Student;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentDAO {
+
+    List<Student> getAll();
     void add(Student student);
     Student findByName(String name);
 
