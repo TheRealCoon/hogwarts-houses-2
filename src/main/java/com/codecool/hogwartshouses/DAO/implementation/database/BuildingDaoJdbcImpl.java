72c761f4664e70ab081bfff1a5f68e8e5e153a8a
T.Sanyi <s.truczkai@gmail.com>
2023-03-18 20:55:08 +0100

    implement BuildingDaoJdbcImpl

--- src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/BuildingDaoJdbcImpl.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/BuildingDaoJdbcImpl.java
@@ -5,8 +5,12 @@ import com.codecool.hogwartshouses.DAO.mapper.BuildingMapper;
 import com.codecool.hogwartshouses.model.Building;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,26 +27,43 @@ public class BuildingDaoJdbcImpl implements BuildingDAO {
 
     @Override
     public List<Building> getAll() {
-        return null;
+        final String sql = "SELECT id, name FROM building;";
+        return template.query(sql, mapper);
     }
 
     @Override
     public void add(Building building) {
-
+        final String sql = "INSERT INTO building (name) VALUES (?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(
+                connection -> {
+                    PreparedStatement ps = connection.prepareStatement(
+                            sql,
+                            Statement.RETURN_GENERATED_KEYS
+                    );
+                    ps.setString(1, building.getName());
+                    return ps;
+                },
+                keyHolder
+        );
+        building.setId(keyHolder.getKey().longValue());
     }
 
     @Override
     public Optional<Building> getById(long id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM building WHERE id = ?;";
+        return template.query(sql, mapper, id).stream().findFirst();
     }
 
     @Override
     public void update(long id, Building building) {
-
+        final String sql = "UPDATE building SET name = ? WHERE id = ?;";
+        template.update(sql, building.getName(), id);
     }
 
     @Override
     public void delete(long id) {
-
+        final String sql = "DELETE FROM building WHERE id = ?;";
+        template.update(sql, id);
     }
 }
