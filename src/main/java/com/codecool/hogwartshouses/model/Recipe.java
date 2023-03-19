25056901f22f9af5626dc4d080fcd58eecea65d5
T.Sanyi <s.truczkai@gmail.com>
2023-03-19 16:45:06 +0100

    add Recipe class

---
+++ src/main/java/com/codecool/hogwartshouses/model/Recipe.java
@@ -0,0 +1,18 @@
+package com.codecool.hogwartshouses.model;
+
+import com.codecool.hogwartshouses.model.enums.Ingredient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+    private String name;
+    private List<Ingredient> ingredients;
+}
