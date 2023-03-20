3af8b91e6e8e57c22f51b46fbeb80cf4ee12afd4
T.Sanyi <s.truczkai@gmail.com>
2023-03-20 02:04:57 +0100

    add methods to PotionDAO

--- src/main/java/com/codecool/hogwartshouses/DAO/PotionDAO.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/PotionDAO.java
@@ -1,6 +1,7 @@
 package com.codecool.hogwartshouses.DAO;
 
 import com.codecool.hogwartshouses.model.Recipe;
+import com.codecool.hogwartshouses.model.enums.Ingredient;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,4 +9,8 @@ import java.util.List;
 @Repository
 public interface PotionDAO {
     List<Recipe> getAllRecipes();
+
+    void addKnownRecipeToStudent(long studentId, long recipeId);
+
+    void add(long studentId, String name, List<Ingredient> ingredients);
 }
