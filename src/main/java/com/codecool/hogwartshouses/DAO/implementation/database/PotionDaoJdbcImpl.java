c4365b085af610e16bdd00f75d5d84962a04ad80
T.Sanyi <s.truczkai@gmail.com>
2023-03-20 02:05:39 +0100

    implement new methods in PotionDaoJdbcImpl

--- src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/PotionDaoJdbcImpl.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/implementation/database/PotionDaoJdbcImpl.java
@@ -6,8 +6,12 @@ import com.codecool.hogwartshouses.DAO.mapper.RecipeMapper;
 import com.codecool.hogwartshouses.model.Recipe;
 import com.codecool.hogwartshouses.model.enums.Ingredient;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -28,6 +32,37 @@ public class PotionDaoJdbcImpl implements PotionDAO {
         List<Recipe> recipes = template.query(sql, recipeMapper);
         recipes.forEach(r -> r.setIngredients(getIngredientsFromDB(r)));
         return recipes;
+    }
+
+    @Override
+    public void addKnownRecipeToStudent(long studentId, long recipeId) {
+        final String sql = "INSERT INTO known_recipes (student_id, recipe_id) VALUES (?, ?);";
+        template.update(sql, studentId, recipeId);
+    }
+
+    @Override
+    public void add(long studentId, String name, List<Ingredient> ingredients) {
+        long recipeId = addRecipe(name);
+        ingredients.forEach(i -> addIngredientToRecipe(recipeId, i.toString()));
+        addKnownRecipeToStudent(studentId, recipeId);
+    }
+
+    private void addIngredientToRecipe(long recipeId, String i) {
+        final String sql = "INSERT INTO ingredients (recipe_id, ingredient) VALUES (?, ?);";
+        template.update(sql, recipeId, i);
+    }
+
+
+    private long addRecipe(String name) {
+        final String sql1 = "INSERT INTO recipe (name) VALUES (?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(connection -> {
+                    PreparedStatement ps = connection.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
+                    ps.setString(1, name);
+                    return ps;
+                },
+                keyHolder);
+        return keyHolder.getKey().longValue();
     }
 
     private List<Ingredient> getIngredientsFromDB(Recipe recipe) {
