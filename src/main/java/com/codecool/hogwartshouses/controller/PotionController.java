0638fc79b602df35dab344863021fa124615b868
T.Sanyi <s.truczkai@gmail.com>
2023-03-20 02:07:35 +0100

    implement methods in PotionController

--- src/main/java/com/codecool/hogwartshouses/controller/PotionController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/PotionController.java
@@ -1,23 +1,45 @@
 package com.codecool.hogwartshouses.controller;
 
+import com.codecool.hogwartshouses.model.enums.Ingredient;
 import com.codecool.hogwartshouses.service.PotionService;
+import com.codecool.hogwartshouses.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/potions")
 public class PotionController {
-    private final PotionService service;
+    private final PotionService potionService;
+    private final StudentService studentService;
 
-    public PotionController(PotionService service) {
-        this.service = service;
+    public PotionController(PotionService potionService, StudentService studentService) {
+        this.potionService = potionService;
+        this.studentService = studentService;
     }
 
     @GetMapping
-    public String getAllRecipes(Model model){
-        model.addAttribute("recipes", service.getAllRecipes());
+    public String getAllRecipes(Model model) {
+        model.addAttribute("recipes", potionService.getAllRecipes());
         return "recipes";
+    }
+
+    @GetMapping("/{studentId}")
+    public String getRecipesKnownByStudent(@PathVariable long studentId, Model model) {
+        model.addAttribute("allIngredients", Ingredient.getStringValues());
+        model.addAttribute("student", studentService.getById(studentId));
+        return "student_recipes";
+    }
+
+    @PostMapping("/{studentId}")
+    public String addRecipeCreatedByStudent(
+            @PathVariable long studentId,
+            @RequestParam("name") String name,
+            @RequestParam("potion_ingredients") List<String> ingredients,
+            @RequestHeader("Referer") String referer) {
+        potionService.add(studentId, name, ingredients);
+        return "redirect:" + referer;
     }
 }
