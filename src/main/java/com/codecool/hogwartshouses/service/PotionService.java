8de131c38ec67a98b1557bd46d250b44650cbfd6
T.Sanyi <s.truczkai@gmail.com>
2023-03-20 02:06:12 +0100

    implement add() in PotionService

--- src/main/java/com/codecool/hogwartshouses/service/PotionService.java
+++ src/main/java/com/codecool/hogwartshouses/service/PotionService.java
@@ -1,21 +1,47 @@
 package com.codecool.hogwartshouses.service;
 
 import com.codecool.hogwartshouses.DAO.PotionDAO;
+import com.codecool.hogwartshouses.DAO.StudentDAO;
 import com.codecool.hogwartshouses.model.Recipe;
+import com.codecool.hogwartshouses.model.enums.Ingredient;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class PotionService {
-private final PotionDAO potionDAO;
+    private final PotionDAO potionDAO;
+    private final StudentDAO studentDAO;
 
-    public PotionService(PotionDAO potionDAO) {
+    public PotionService(PotionDAO potionDAO, StudentDAO studentDAO) {
         this.potionDAO = potionDAO;
+        this.studentDAO = studentDAO;
     }
 
-    public List<Recipe> getAllRecipes(){
+    public List<Recipe> getAllRecipes() {
         return potionDAO.getAllRecipes();
+    }
+
+    public void add(long studentId, String name, List<String> stringIngredients) {
+        List<Ingredient> ingredients = parseIngredientList(stringIngredients);
+        getRecipeIfAlreadyExists(ingredients, studentDAO.getById(studentId).orElseThrow().getKnownRecipes())
+                .ifPresentOrElse(
+                        r -> potionDAO.addKnownRecipeToStudent(studentId, r.getId()),
+                        () -> potionDAO.add(studentId, name, ingredients)
+                );
+    }
+
+    private List<Ingredient> parseIngredientList(List<String> stringIngredients) {
+        return stringIngredients.stream()
+                .map(Ingredient::getByStringValue)
+                .collect(Collectors.toList());
+    }
+
+    private Optional<Recipe> getRecipeIfAlreadyExists(List<Ingredient> ingredients, Set<Recipe> recipesOfStudent) {
+        return recipesOfStudent.stream()
+                .filter(r -> ingredients.equals(r.getIngredients()))
+                .findFirst();
     }
 
 }
