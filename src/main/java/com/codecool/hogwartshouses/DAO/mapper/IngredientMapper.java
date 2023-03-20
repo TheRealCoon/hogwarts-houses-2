38f25f5e5281fd100045a278ded0e6c1420e0e21
T.Sanyi <s.truczkai@gmail.com>
2023-03-20 01:56:50 +0100

    rework IngredientMapper

--- src/main/java/com/codecool/hogwartshouses/DAO/mapper/IngredientMapper.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/mapper/IngredientMapper.java
@@ -6,22 +6,12 @@ import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 @Component
 public class IngredientMapper implements RowMapper<Ingredient> {
     @Override
     public Ingredient mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return Arrays.stream(Ingredient.values()).filter(i -> equals(rs, i))
-                .findFirst()
-                .orElse(null);
+        return Ingredient.getByStringValue(rs.getString("ingredient"));
     }
 
-    private static boolean equals(ResultSet rs, Ingredient i) {
-        try {
-            return rs.getString("ingredient").equals(i.toString());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
